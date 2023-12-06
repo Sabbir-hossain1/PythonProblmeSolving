180fef6d8326c404e07314a0d6fe9a406d97c30d
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2023-12-06 11:05:27 +0600

    Valid Parenthese problem solved

---
+++ validParentheses.py
@@ -0,0 +1,24 @@
+"""
+Problem:
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+"""
+
+class Solution:
+    def isValid(self, strs):
+        stack = []
+        for c in strs:
+            if c == '(':
+                stack.append(')')
+            elif c == '{':
+                stack.append('}')
+            elif c == '[':
+                stack.append(']')
+            elif not stack or stack.pop() != c:
+                return False
+        return not stack
